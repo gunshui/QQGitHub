c60f36b147b8756398211b4057f9a619fe5fd298
gunshui <minqinyang@163.com>
2015-05-28 16:25:55 +0800

    Initial Commit

---
+++ QQGitHubTests/QQGitHubTests.m
@@ -0,0 +1,40 @@
+//
+//  QQGitHubTests.m
+//  QQGitHubTests
+//
+//  Created by digime on 15/5/28.
+//  Copyright (c) 2015年 digime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface QQGitHubTests : XCTestCase
+
+@end
+
+@implementation QQGitHubTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
